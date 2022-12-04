72a74c3248c78902dc70c80876c55d125f484d5d
Jules Fouchy <jules.fouchy@ntymail.com>
2022-12-04 11:29:59 +0100

    ❄️ Day 4 | Challenge 2

--- 04.py
+++ 04.py
@@ -19,12 +19,12 @@ def parse_range(range_as_str: str):
     return Range(next(as_int), next(as_int))
 
 
-def one_range_contains_the_other(range_pair: List[Range]):
-    def is_subrange(a: Range, b: Range):
-        return a.begin >= b.begin and a.end <= b.end
+def one_range_overlaps_the_other(range_pair: List[Range]):
+    def first_element_is_inside_other(a: Range, b: Range):
+        return a.begin >= b.begin and a.begin <= b.end
 
-    return (is_subrange(range_pair[0], range_pair[1])
-            or is_subrange(range_pair[1], range_pair[0]))
+    return (first_element_is_inside_other(range_pair[0], range_pair[1])
+            or first_element_is_inside_other(range_pair[1], range_pair[0]))
 
 
 def main(filepath: str):
@@ -35,7 +35,7 @@ def main(filepath: str):
               pair
               | map(parse_range)
               ))
-        | where(one_range_contains_the_other)
+        | where(one_range_overlaps_the_other)
         | count_elements
     )
 
