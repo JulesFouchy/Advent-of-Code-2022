5b5241ffe293912514fb3454b3a38546a81ec79b
Jules Fouchy <jules.fouchy@ntymail.com>
2022-12-10 15:06:35 +0100

    🔵 Day 10 | Challenge 2

--- 10.py
+++ 10.py
@@ -13,20 +13,21 @@ def convert_to_add_instructions(line: str):
 
 def main(filepath: str):
     # List of values during each cycle
-    output(
+    out = (
         enumerate(
-            # Add two initial noops to emulate the initial state and offset our indices by 1 and use a "indices start at 1" counting
-            itertools.chain(["noop"]*2, lines(filepath))
+            # Add one initial noop to emulate the initial state
+            itertools.chain(["noop"], lines(filepath))
             | map(convert_to_add_instructions)
             | traverse
             | accumulate(operator.add)
             | map(lambda x: x+1)  # The register is initially set to one
         )
-        | where(lambda x: (x[0] - 20) % 40 == 0)  # Select the 20th, 60th etc.
-        | tee
-        | map(lambda x: x[0] * x[1])  # Calculate signal strength
-        | apply(sum)
+        | groups_of(40)
     )
+    for line in out:
+        for p, s in line:
+            print("❄️" if abs(p % 40-s) <= 1 else " ", end="")
+        print()
 
 
 main(f"{day_number(__file__)}.input")
