e02524246e1bbfcab24f8603808051d515d7488a
Jules Fouchy <jules.fouchy@ntymail.com>
2022-12-15 21:17:50 +0100

    🚇 Day 15 | Challenge 2

--- 15.py
+++ 15.py
@@ -16,29 +16,74 @@ class Sensor:
     closest_beacon_pos: Pos
 
 
+@dataclass
+class Diamond:
+    center: Pos
+    dist: int
+
+
+@dataclass
+class Segment:
+    start: int
+    stop: int
+
+
+@dataclass
+class SegmentList:
+    segments: List[Segment]
+    row: int
+
+
+def is_empty(segment: Segment) -> bool:
+    return segment.start > segment.stop
+
+
+def subtract(diamond: Diamond, segments: SegmentList) -> None:
+    remaining_dist = diamond.dist - abs(diamond.center.y - segments.row)
+    if remaining_dist < 0:
+        return
+    new_segs = []
+    for seg in segments.segments:
+        sub = subtract_segments(
+            seg,
+            Segment(
+                start=diamond.center.x - remaining_dist,
+                stop=diamond.center.x + remaining_dist,
+            ),
+        )
+        new_segs.extend(sub)
+    segments.segments = list(new_segs | where(lambda s: not is_empty(s)))
+
+
+def subtract_segments(seg1: Segment, seg2: Segment) -> List[Segment]:
+    return [
+        Segment(seg1.start, min(seg2.start-1, seg1.stop)),
+        Segment(max(seg2.stop+1, seg1.start), seg1.stop),
+    ]
+
+
 def main(filepath: str):
     sensors: List[Sensor] = list(
         lines(filepath)
         | map(parse_sensor)
     )
 
-    row_to_check = 2000000
-
-    pos_where_beacon_can_not_be = set()
-    for sensor in sensors:
-        dist = manhattan_distance(sensor.pos, sensor.closest_beacon_pos)
-        dist_to_row = abs(sensor.pos.y - row_to_check)
-        for x in range(0, dist-dist_to_row + 1):
-            for x_sign in [-1, 1]:
-                pos = Pos(x*x_sign+sensor.pos.x, row_to_check)
-                if pos == sensor.closest_beacon_pos:
-                    continue
-                pos_where_beacon_can_not_be.add(pos)
-
-    output(
-        pos_where_beacon_can_not_be
-        | count_elements
-    )
+    for row in range(4000000 + 1):
+        if row % 10000 == 0:
+            print(row / 4000000)
+        subtraction = SegmentList([Segment(0, 4000000)], row)
+        for sensor in sensors:
+            subtract(
+                Diamond(
+                    center=sensor.pos,
+                    dist=manhattan_distance(
+                        sensor.pos, sensor.closest_beacon_pos),
+                ),
+                subtraction,
+            )
+        if len(subtraction.segments) > 0:
+            print(4000000 * subtraction.segments[0].start + row)
+            break
 
 
 def manhattan_distance(p1: Pos, p2: Pos) -> int:
@@ -57,4 +102,6 @@ def parse_sensor(line: str) -> Sensor:
     )
 
 
-main(f"{day_number(__file__)}.input")
+main(
+    f"{day_number(__file__)}.input"
+)
